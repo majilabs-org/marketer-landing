097727100c64175a0f173899004de8097cf36c23
Armedin <armedin@majilabs.com>
2026-07-14 23:28:33 +0100

    Comment out section

--- app/page.tsx
+++ app/page.tsx
@@ -180,7 +180,7 @@ export default function Home() {
       <CaseStudiesSection />
       <FaqSection />
       <TrustedBy />
-      <div>
+      {/* <div>
         <div className="border-t border-[#e4e4e7] h-[2px] bg-white" />
         <div className="pt-[80px] w-full flex-1 px-6 max-w-[1200px] mx-auto relative border-l border-r border-[#e4e4e7]">
           <div className="mx-auto w-min relative p-3 pb-0 rounded-t-[21px] overflow-hidden border border-b-0 bg-[#18181B]/2 border-[#e4e4e7]">
@@ -206,7 +206,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <BenefitsSection />
       <Footer />
